fe7ef12f5b0ab3a49cd6222e98cbe45b567d34fa
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2024-09-09 18:20:57 +0700

    fix test for class Head

--- model/head.py
+++ model/head.py
@@ -1,6 +1,10 @@
 import torch
 from torch import nn
 
-class Head(nn.Module):
-    def __init__(self):
-        print('hello')
+# class Head(nn.Module):
+#     def __init__(self):
+#         print('hello')
+
+t = torch.randn(1,2,3)
+
+print(t.shape)
--- test/test_head.py
+++ test/test_head.py
@@ -9,33 +9,32 @@ from head import *
 class TestHead(unittest.TestCase):
     def test_head_init(self):
         head = Head(head_size = 16, n_embedding = 32)
-        self.assertEquals(head.head_size, 16)
-        self.assertEquals(head.n_embedding, 32)
+        self.assertEqual(head.head_size, 16)
+        self.assertEqual(head.n_embedding, 32)
     def test_head_forward(self):
         B,T,C = 4,8,32
         H = 16
         x = torch.randn(B,T,C)
         head = Head(head_size = H, n_embedding = C)
+        tril = torch.tril(torch.ones(T,T))
         k = head.key(x) # (B,T,C) @ (B,C,H) = (B,T,H)
         q = head.query(x) # (B,T,C) @ (B,C,H) = (B,T,H)
         v = head.value(x) # (B,T,C) @ (B,C,H) = (B,T,H)
         weight = q @ k.transpose(1,2)*H**(-0.5) # (B,T,H) @ (B,H,T) = (B,T,T)
-        tril = torch.tril(torch.ones(T,T))
-        weight = weight.masked_fill_(tril == 0, 0)
+        weight = weight.masked_fill_(tril == 0, float('-inf'))
         weight = head.softmax(weight, -1)
-        self.assertEquals(C, head.n_embedding)
-        self.assertEquals(k.shape, (H,head.head_size))
-        self.assertEquals(q.shape, (H,head.head_size))
-        self.assertEquals(v.shape, (H,head.head_size))
-        self.assertEquals(k.transpose(1,2).shape, (head.head_size,x[1]))
-        self.assertEquals(weight.shape, (T,T))
-        self.assertEquals(weight,torch.tril(weight))
+        self.assertEqual(C, head.n_embedding)
+        self.assertEqual(k.shape, torch.Size([B,T,H]))
+        self.assertEqual(q.shape, torch.Size([B,T,H]))
+        self.assertEqual(v.shape, torch.Size([B,T,H]))
+        self.assertEqual(k.transpose(1,2).shape, torch.Size([B,H,T]))
+        self.assertEqual(weight.shape, torch.Size([B,T,T]))
+        self.assertEqual(weight,torch.tril(weight))
         # test sum = 1
         for batch in weight:
             for row in batch:
-                self.assertEquals(sum(row),1)
-
-        weight = weight.masked_fill_(tril == 0, float('-inf'))
+                self.assertAlmostEqual(torch.sum(row).item(), 1)
+        weight = head.dropout(weight)
         out = weight @ v #(B,T,T) @ (B,T,H) = (B,T,H)
-        self.assertEquals(out.shape,(B,T,H))
+        self.assertEqual(out.shape,torch.Size([B,T,H]))
 
