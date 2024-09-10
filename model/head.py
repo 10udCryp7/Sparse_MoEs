f7b5f3d611beeb519480c9d6041336eb20e7cc74
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2024-09-10 12:30:09 +0700

    refractor code and add comment

--- model/head.py
+++ model/head.py
@@ -1,25 +1,38 @@
 import torch
 from torch import nn
 
+
 class Head(nn.Module):
     def __init__(self, head_size, n_embedding):
-        super(Head,self).__init__()
+        super(Head, self).__init__()
         self.head_size = head_size
         self.n_embedding = n_embedding
         self.query = nn.Linear(self.n_embedding, self.head_size, bias=False)
         self.key = nn.Linear(self.n_embedding, self.head_size, bias=False)
         self.value = nn.Linear(self.n_embedding, self.head_size, bias=False)
+        '''
+        if we assign value for tril hear instead, 
+        we need to use register_buffer as buffer wont be returned in model.parameters() so that optimizer wont update tril.
+        '''
         self.tril = torch.tril
         self.softmax = nn.Softmax(dim=-1)
         self.dropout = nn.Dropout()
-    def forward(self,x):
+
+    def forward(self, x):
         q = self.query(x)
         k = self.key(x)
         v = self.value(x)
         tril = self.tril(torch.ones(x.shape[1], x.shape[1]))
-        weight = q @ k.transpose(1,2)*self.head_size**(-0.5)
+        weight = q @ k.transpose(1, 2)*self.head_size**(-0.5)
         weight = weight.masked_fill(tril == 0, float('-inf'))
         weight = self.softmax(weight)
         weight = self.dropout(weight)
         out = weight @ v
         return out
+
+
+model = Head(32, 64)
+
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print(name, param.data)
--- test/test_head.py
+++ test/test_head.py
@@ -1,37 +1,37 @@
+from head import *
+import unittest
+import torch
 import sys
 sys.path.insert(0, 'model')
 
 
-import torch
-import unittest
-from head import *
-
 class TestHead(unittest.TestCase):
     def test_head_init(self):
-        head = Head(head_size = 16, n_embedding = 32)
+        head = Head(head_size=16, n_embedding=32)
         self.assertEqual(head.head_size, 16)
         self.assertEqual(head.n_embedding, 32)
+
     def test_head_forward(self):
-        B,T,C = 4,8,32
+        B, T, C = 4, 8, 32
         H = 16
-        x = torch.randn(B,T,C)
-        head = Head(head_size = H, n_embedding = C)
-        tril = torch.tril(torch.ones(T,T))
-        q = head.query(x) # (B,T,C) @ (B,C,H) = (B,T,H)
-        k = head.key(x) # (B,T,C) @ (B,C,H) = (B,T,H)
-        v = head.value(x) # (B,T,C) @ (B,C,H) = (B,T,H)
-        weight = q @ k.transpose(1,2)*H**(-0.5) # (B,T,H) @ (B,H,T) = (B,T,T)
+        x = torch.randn(B, T, C)
+        head = Head(head_size=H, n_embedding=C)
+        tril = torch.tril(torch.ones(T, T))
+        q = head.query(x)  # (B,T,C) @ (B,C,H) = (B,T,H)
+        k = head.key(x)  # (B,T,C) @ (B,C,H) = (B,T,H)
+        v = head.value(x)  # (B,T,C) @ (B,C,H) = (B,T,H)
+        weight = q @ k.transpose(1, 2)*H**(-0.5)  # (B,T,H) @ (B,H,T) = (B,T,T)
         weight = weight.masked_fill(tril == 0, 0)
         self.assertEqual(C, head.n_embedding)
-        self.assertEqual(q.shape, torch.Size([B,T,H]))
-        self.assertEqual(k.shape, torch.Size([B,T,H]))
-        self.assertEqual(v.shape, torch.Size([B,T,H]))
-        self.assertEqual(k.transpose(1,2).shape, torch.Size([B,H,T]))
-        self.assertEqual(weight.shape, torch.Size([B,T,T]))
+        self.assertEqual(q.shape, torch.Size([B, T, H]))
+        self.assertEqual(k.shape, torch.Size([B, T, H]))
+        self.assertEqual(v.shape, torch.Size([B, T, H]))
+        self.assertEqual(k.transpose(1, 2).shape, torch.Size([B, H, T]))
+        self.assertEqual(weight.shape, torch.Size([B, T, T]))
         print(f'{weight =}')
         print(f'{head.tril(weight) =}')
         self.assertTrue(torch.equal(weight, head.tril(weight)))
-    
+
         weight = weight.masked_fill(tril == 0, float('-inf'))
         weight = head.softmax(weight)
         # test sum = 1
@@ -40,9 +40,10 @@ class TestHead(unittest.TestCase):
                 self.assertAlmostEqual(torch.sum(row).item(), 1.0, delta=1e4)
 
         weight = head.dropout(weight)
-        out = weight @ v #(B,T,T) @ (B,T,H) = (B,T,H)
-        self.assertEqual(out.shape,torch.Size([B,T,H]))
-        self.assertEqual(out.shape,head.forward(x).shape)
+        out = weight @ v  # (B,T,T) @ (B,T,H) = (B,T,H)
+        self.assertEqual(out.shape, torch.Size([B, T, H]))
+        self.assertEqual(out.shape, head.forward(x).shape)
+
 
 if __name__ == '__main__':
     unittest.main(argv=[''], verbosity=2, exit=False)
