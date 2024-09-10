2c5507ff08d5b5ddc344e99e1eac134e6fe45a26
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2024-09-10 16:45:23 +0700

    refractor head_attention

--- test/test_head_attention.py
+++ test/test_head_attention.py
@@ -1,4 +1,4 @@
-from head import *
+from head_attention import *
 import unittest
 import torch
 import sys
@@ -8,6 +8,7 @@ sys.path.insert(0, 'model')
 class TestHead(unittest.TestCase):
     def test_head_init(self):
         head = Head(head_size=16, n_embedding=32)
+        # check initialization
         self.assertEqual(head.head_size, 16)
         self.assertEqual(head.n_embedding, 32)
 
@@ -23,22 +24,22 @@ class TestHead(unittest.TestCase):
         weight = q @ k.transpose(1, 2)*H**(-0.5)  # (B,T,H) @ (B,H,T) = (B,T,T)
         weight = weight.masked_fill(tril == 0, 0)
         self.assertEqual(C, head.n_embedding)
+        # check shape of q,k,v
         self.assertEqual(q.shape, torch.Size([B, T, H]))
         self.assertEqual(k.shape, torch.Size([B, T, H]))
         self.assertEqual(v.shape, torch.Size([B, T, H]))
+        # check transposition
         self.assertEqual(k.transpose(1, 2).shape, torch.Size([B, H, T]))
+        # check masking for weight
         self.assertEqual(weight.shape, torch.Size([B, T, T]))
-        print(f'{weight =}')
-        print(f'{head.tril(weight) =}')
         self.assertTrue(torch.equal(weight, head.tril(weight)))
-
         weight = weight.masked_fill(tril == 0, float('-inf'))
         weight = head.softmax(weight)
         # test sum = 1
         for batch in weight:
             for row in batch:
                 self.assertAlmostEqual(torch.sum(row).item(), 1.0, delta=1e4)
-
+        # check output
         weight = head.dropout(weight)
         out = weight @ v  # (B,T,T) @ (B,T,H) = (B,T,H)
         self.assertEqual(out.shape, torch.Size([B, T, H]))
